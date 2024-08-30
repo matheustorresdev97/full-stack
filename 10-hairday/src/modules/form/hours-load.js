2d45c9c76fcd8482dd4bff8652c94950da4d39e6
MatheusTorresDev97 <matheustorresdev@gmail.com>
2024-08-30 12:59:42 -0300

    manipulando o form

---
+++ 10-hairday/src/modules/form/hours-load.js
@@ -0,0 +1,48 @@
+import dayjs from "dayjs"
+import { openingHours } from "../../utils/opening-hours"
+import { hoursClick } from "./hours-click"
+
+const hours = document.getElementById("hours")
+
+export function hoursLoad({date}) {
+    const opening = openingHours.map((hour) => {
+        // Recupera somente a hora.
+        const [scheduleHour] = hour.split(":")
+
+        // Adiciona a hora na date e verificar se está no passado.
+        const isHourPast = dayjs(date).add(scheduleHour, "hour").isAfter(dayjs)
+
+
+      return {
+        hour,
+        available: isHourPast
+      }
+    })
+
+    opening.forEach(({hour, available}) => {
+        const li = document.createElement("li")
+
+        li.classList.add("hour")
+        li.classList.add(available ? "hour-available" : "hour-unavailable")
+
+        li.textContent = hour
+
+        if(hour === "9:00"){
+            hourDeaderAdd("Manhã")
+        }else if(hour === "13:00") {
+            hourDeaderAdd("Tarde")
+        } else if (hour === "18:00") {
+            hourDeaderAdd("Noite")
+        }
+
+        hours.append(li)
+    })
+    hoursClick()
+}
+
+function hourDeaderAdd(title) {
+    const header = document.createElement('li')
+    header.classList.add("hour-period")
+    header.textContent = title
+    hours.append(header)
+}
